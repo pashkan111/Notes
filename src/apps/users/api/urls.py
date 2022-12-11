87f6dd9cd718e35a21e1f9b17c0f2c85f76cd47f
pashkan111 <kozhevnikov0001@yandex.ru>
2022-12-11 14:41:19 +0600

    Done with register on client

--- src/apps/users/api/urls.py
+++ src/apps/users/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TokenObtainPairView, TokenRefreshView
+from .views import TokenObtainPairView, RegisterView, TokenRefreshView
 
 urlpatterns = [
     path("", TokenObtainPairView.as_view(), name="token_obtain"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register", RegisterView.as_view(), name="register"),
 ]
