e169e8285a1695c48609fd5940a6bf5e9e4ddbda
pashkan111 <kozhevnikov0001@yandex.ru>
2022-12-12 21:52:44 +0600

    changed serializers

--- src/apps/notes_app/api/serializers.py
+++ src/apps/notes_app/api/serializers.py
@@ -3,49 +3,19 @@ from rest_framework import serializers
 from apps.django_common.exceptions import NoCategoryError
 
 
-class SerializerWriteAllowMethodField(serializers.SerializerMethodField):
-    def __init__(self, method_name=None, **kwargs):
-        super().__init__(method_name, **kwargs)
-        self.read_only = False
-
-    def to_internal_value(self, data):
-        return {self.field_name: data}
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
         fields = ('id', 'name')
 
+class CategoryNoteSerializer(serializers.CharField):
+    def to_internal_value(self, data):
+        cat = models.Category.objects.get(id=data)
+        return cat
 
 class NoteSerializer(serializers.ModelSerializer):
-    category = SerializerWriteAllowMethodField()
+    category = CategoryNoteSerializer()
     class Meta:
         model = models.Note
         exclude = ('author',)
-
-    def get_category(self, obj):
-        return obj.category.name
-
-    def create(self, validated_data):
-        category_name = validated_data.pop('category', None)
-        category = models.Category.objects.filter(
-            name=category_name
-        ).first()
-        if category is None:
-            raise NoCategoryError
-
-        note = models.Note.objects.create(
-            **validated_data, category=category
-        )
-        return note
-
-    def update(self, instance, validated_data):
-        category_name = validated_data.pop('category', None)
-        category = models.Category.objects.filter(
-            name=category_name
-        ).first()
-        if category is not None:
-            validated_data.setdefault('category_id', category.id)
-            return super().update(instance, validated_data)
-        return super().update(instance, validated_data)
